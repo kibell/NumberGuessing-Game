214116e048dc023db623ec8e0a46556fd803ef32
Kitrick Bell <kitrickd.bell@gmail.com>
2021-06-10 13:47:37 -0500

    WE have connected our external JS sheet with our html

---
+++ assest/javascript/game.js
@@ -0,0 +1,12 @@
+//  Variable , counter , tries counter, wins counter lose counter, Guess counter
+
+
+//Get random number
+
+//get input from user
+
+//compare that input with the random number
+
+//show the output
+
+
